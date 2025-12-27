02da32fb9698577a0451b5db7ed5892132872948
Sachini Malindi <sachini@Sachinis-iMac.local>
2025-12-27 21:57:41 +1030

    feat(api): init Spring Boot API with postgres + health endpoint

---
+++ services/api/src/main/java/com/servicebooker/api/ServicebookerApiApplication.java
@@ -0,0 +1,13 @@
+package com.servicebooker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebookerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebookerApiApplication.class, args);
+	}
+
+}
---
+++ services/api/src/test/java/com/servicebooker/api/ServicebookerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicebooker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
